b8169f359bf705f723d3c41f75af7eea35d277a1
norjuste <norjuste@gmail.com>
2013-05-16 13:25:18 +0300

    Dovanų!

---
+++ OSas4/src/resources/ResursuSarasuSarasas.java
@@ -0,0 +1,8 @@
+package resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ResursuSarasuSarasas {
+	public static List<ResursuSarasas> list = new ArrayList<ResursuSarasas>();
+}
