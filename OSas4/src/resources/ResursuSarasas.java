775074e6f131981906c755aa2fc47b13a12c882b
norjuste <norjuste@gmail.com>
2013-05-16 13:20:46 +0300

    naujas sąrašas

--- OSas4/src/resources/ResursuSarasas.java
+++ OSas4/src/resources/ResursuSarasas.java
@@ -4,5 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ResursuSarasas {
+	public static ResourceDescriptor resourceDescriptor;
 	public static List<ProcessNeedsResource> list = new ArrayList<ProcessNeedsResource>();
 }
