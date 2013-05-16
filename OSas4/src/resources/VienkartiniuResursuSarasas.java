cdb4f93eb3d58362db96fb5d6644f9b6c8ffa3c7
norjuste <norjuste@gmail.com>
2013-05-16 13:43:23 +0300

    Vienkartinių resursu sarasas

---
+++ OSas4/src/resources/VienkartiniuResursuSarasas.java
@@ -0,0 +1,10 @@
+package resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VienkartiniuResursuSarasas {
+	public static List<ResourceDescriptor> resourceList = new ArrayList<ResourceDescriptor>();
+	public static String vardas;
+	public static List<ProcessNeedsResource> processList = new ArrayList<ProcessNeedsResource>();
+}
---
+++ OSas4/src/resources/VienkartiniuResursuSarasuSarasas.java
@@ -0,0 +1,8 @@
+package resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VienkartiniuResursuSarasuSarasas {
+	public static List<VienkartiniuResursuSarasas> list = new ArrayList<VienkartiniuResursuSarasas>();
+}
