b8f0888c1a784e06d22e4689f4f8df841fd7291c
Skaireka <skaireka@gmail.com>
2013-05-17 12:53:57 +0300

    dar dvi INFO klases

---
+++ OSas4/src/resourcesINFO/ProgramosInfoHDD.java
@@ -0,0 +1,15 @@
+package resourcesINFO;
+
+public class ProgramosInfoHDD {
+	public String name; // programos vardas
+	int nr;	// programos identifikacinis numeris in HDD
+	int memory; // kiek bloku programa uzima in HDD
+	
+	public ProgramosInfoHDD(String name, int nr, int memory) {
+		this.name = name;
+		this.nr = nr;
+		this.memory = memory;
+		
+	}
+
+}
