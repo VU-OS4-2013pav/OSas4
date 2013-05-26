2b80e499693a6427ca182795fe660fa85dd79c33
norjuste <norjuste@gmail.com>
2013-05-26 09:52:43 +0300

    Šiek tiek pradėtas rašyti Interrupt. Dabar negalima įvedinėti per kelis kartus, viską reik suvest per vieną!!! (Tai pasikeitė po vakar)

--- OSas4/src/Procesai/Interrupt.java
+++ OSas4/src/Procesai/Interrupt.java
@@ -1,7 +1,10 @@
 package Procesai;
 
 import os.Primityvai;
+import os.Statiniai;
 import os.Statiniai.VRstring;
+import resources.ResourceDescriptor;
+import resources.VRSS;
 
 
 public class Interrupt extends ProcessBase {
@@ -13,6 +16,23 @@ public class Interrupt extends ProcessBase {
 			Primityvai.prasytiResurso(VRstring.Pertraukimo_ivykis, nameI, 1);
 			break;
 		case 1:
+			//Ádentifikuoja virtual machine tëvà JG
+			ResourceDescriptor resursas = null;
+			//randam konkreèiai ðitam procesui skirtà resursà
+			for (int i = 0; i < VRSS.list.get(Statiniai.VRint.Pertraukimo_ivykis).resourceList.size(); i++)
+				if (VRSS.list.get(Statiniai.VRint.Pertraukimo_ivykis).resourceList.get(i).nameI == resursai.get(0).nameI) {
+					resursas = VRSS.list.get(Statiniai.VRint.Pertraukimo_ivykis).resourceList.get(i);
+					break; //radom ko reikia
+				}
+			
+			if (resursas == null) {
+				System.out.println("Nerastas resursas Interrupt!");
+			}
+			
+			int jgVardas = (Integer)((Object[])resursas.info.o)[0]; //Iðsipleðiam JG 
+			
+			//Apdorojami pertraukimo ávykiai
+			
 			break;
 		}
 		
