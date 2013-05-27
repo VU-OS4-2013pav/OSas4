8e78073b27cfdd0122a4233864ce73f6ca14e414
Skaireka <skaireka@gmail.com>
2013-05-27 20:56:52 +0300

    sutvarkytas MG

--- OSas4/src/Procesai/MainGovernor.java
+++ OSas4/src/Procesai/MainGovernor.java
@@ -18,6 +18,7 @@ public class MainGovernor extends ProcessBase {
 		switch(vieta) {
 		case 0:
 			vieta++;
+			System.out.println("AS ESU CASE 0!!!!!!!!!!!!!!!!!!!!!");
 			Primityvai.prasytiResurso(VRstring.MainGovernor_pazadinimas, nameI, 1);
 			return;
 		case 1:
@@ -25,19 +26,19 @@ public class MainGovernor extends ProcessBase {
 
 			// surandame main governor pazadinimas resursa proceso sarase
 			for (int i = 0; i < this.resursai.size(); i++) {
-				if (this.resursai.get(i).nameO == VRstring.MainGovernor_pazadinimas) {
+				if (this.resursai.get(i).nameO.equals(VRstring.MainGovernor_pazadinimas)) {
 					res = i;
 					break;
 				}
 			}
-
+			System.out.println("AS ESU CASE 1 po MG pazadinimo radimo!!!!!!!!!!!!!!!!!!!!!! i:"+res);
 			if (res >= 0) { // jeigu toks resursas egzistuoja...
 				// issitraukiam ta resursa
 				ResourceDescriptor rd = null;
 				for (int i = 0; i < VRSS.list.get(VRint.MainGovernor_pazadinimas).resourceList.size(); i++) {
 					if (VRSS.list.get(VRint.MainGovernor_pazadinimas).resourceList.get(i).nameI == this.resursai.get(res).nameI) {
 						rd = VRSS.list.get(VRint.MainGovernor_pazadinimas).resourceList.get(i);
-						
+						Primityvai.naikintiResursa(VRSS.list.get(VRint.MainGovernor_pazadinimas).resourceList.get(i).nameI);
 					}
 				}
 
@@ -51,17 +52,17 @@ public class MainGovernor extends ProcessBase {
 						INFOv inf = new INFOv();
 						((Object[])inf.o)[0] = ((Object[])rd.info.o)[1];
 						vieta = 0;
-						Primityvai.naikintiResursa(rd.nameI);
+						//Primityvai.naikintiResursa(rd.nameI);
 						Primityvai.sukurtiResursa(VRstring.Info_apie_nauja_VM, true, this.nameI, inf);
 						return;
 					}
 					else {
 						vieta = 0;
-						Primityvai.naikintiResursa(rd.nameI);
+						//Primityvai.naikintiResursa(rd.nameI);
 
 						//naikinimas
 						Primityvai.naikintiProcesa(((int)((Object[])rd.info.o)[1]), this);
-						
+
 					}
 				}
 				else
@@ -70,6 +71,13 @@ public class MainGovernor extends ProcessBase {
 			else
 				System.out.println("Main Governor klaida. MainGovernor_pazadinimas resursas neegzistuoja.");
 
+			for (int i = 0; i < this.resursai.size(); i++) {
+				if (this.resursai.get(i).nameO.equals(VRstring.MainGovernor_pazadinimas))
+					Primityvai.naikintiResursa(this.resursai.get(i).nameI);
+			}
+			
+			System.out.println("AS ESU CASE 1 galeeeeeeeeeeeeeeeee!!!!!!!!!!!!!!!!!!!!!");
+			vieta = 1;
 			Primityvai.prasytiResurso(VRstring.MainGovernor_pazadinimas, this.nameI, 1);
 			return;
 		}
